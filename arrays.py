c31ff61d359c95e99773ed4f8cb7e8f96789724c
whizikem <wisdomikem@gmail.com>
2026-04-06 00:49:05 +0100

    first commit

---
+++ arrays.py
@@ -0,0 +1,21 @@
+
+new_list = [1, 2, 3]
+
+# store the new_list into another list
+result = new_list[0]
+
+# search for item in list
+if 1 in new_list: print(True)
+
+# using for loop to search
+for n in new_list:
+  if n == 1:
+    print(True)
+
+    break
+
+new_list
+
+# inserting items
+# inserting at 0 means every other items needs to move forward, which is a linear operation
+
---
+++ linked_list_merge_sort.py
@@ -0,0 +1,82 @@
+from linked_list import LinkedList
+
+def merge_sort(linked_list):
+    if linked_list.size() == 1:
+        return linked_list
+    elif linked_list.head is None:
+        return linked_list
+
+    left_half, right_half = split(linked_list)
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
+
+    return merge(left, right)
+
+def split(linked_list):
+    if linked_list is None or linked_list.head is None:
+        left_half = linked_list
+        right_half = None
+        return left_half, right_half
+    else:
+        size = linked_list.size()
+        mid = size // 2
+
+        mid_node = linked_list.node_at_index(mid - 1)
+
+        left_half = linked_list
+        right_half = LinkedList()
+        right_half.head = mid_node.next_node
+        mid_node.next_node = None
+
+        return left_half, right_half
+
+def merge(left, right):
+    merged = LinkedList()
+    merged.add(0)  # dummy node
+    current = merged.head
+
+    left_head = left.head
+    right_head = right.head
+
+    while left_head or right_head:
+        if left_head is None:
+            current.next_node = right_head
+            break
+        elif right_head is None:
+            current.next_node = left_head
+            break
+        else:
+            if left_head.data < right_head.data:
+                current.next_node = left_head
+                left_head = left_head.next_node
+            else:
+                current.next_node = right_head
+                right_head = right_head.next_node
+        current = current.next_node
+
+    merged.head = merged.head.next_node  # remove dummy node
+    return merged
+
+# Add this helper to LinkedList class
+def node_at_index(self, index):
+    current = self.head
+    position = 0
+    while current and position < index:
+        current = current.next_node
+        position += 1
+    return current
+
+# Monkey patch method (if not already in LinkedList)
+LinkedList.node_at_index = node_at_index
+
+# TESTING
+l = LinkedList()
+l.add(10)
+l.add(2)
+l.add(44)
+l.add(15)
+l.add(200)
+
+print("Unsorted list:", l)
+sorted_linked_list = merge_sort(l)
+print("Sorted list:", sorted_linked_list)
